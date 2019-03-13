3cd2c275e1bfb20ef4ee9f96f78ef989e1dd164a
jleyland96 <jack.leyland@durham.ac.uk>
2019-03-13 11:37:25 +0000

    re-verified

--- verify.py
+++ verify.py
@@ -1,14 +1,14 @@
 import ecdsa
 
 # put the hex of your public key in the line below
-vk_string="56bab87d26a2a95f0201a3ebc15b8269a316ad7f23e483ef6324242f36ff1b5a8329a28dfc72625cffae2498d0281b37d0261b8e614c62ef00c79e7d664850fb"
+vk_string="f13d1d92e0bffcdbee0309ada8f882572842e8b3f639704605872fde6770e552e26e62c5724c14a3deaa260593db03227d8830179529f73da8704a3e1abedc2c"
 vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(vk_string), ecdsa.SECP256k1)
 
 # the generation signature is 9737957703d4eb54efdff91e15343266123c5f15aaf033292c9903015af817f1
-message = b'Hello World'
+message = b'9737957703d4eb54efdff91e15343266123c5f15aaf033292c9903015af817f1'
 
 # put your signature for Hello World in the line below
-sig_hex = "aec300825740810dc91c57ab5bfe948f07298c0d49327e91435b88b3194f5ce388dcbe6fbbd3709b093a3546c265fa70a79751a9643b62b96b0eb7ebcff8e087"
+sig_hex = "08d7cbf6883ca98981602dbd41380c02a3360c82e45922b9bda0be30da51caaa2c6162aae7778a30e1ac768ac6b86a3deefb6ebabcda7fb5269fdfdb75c24b26"
 sig = bytes.fromhex(sig_hex)
 
 print("Checking signature")
